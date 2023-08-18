2eacc0d388e6455b42c335327058ce6e37fc8acd
katsuya-ominami <katsuya.ominami@gmail.com>
2023-08-18 23:37:17 +0900

    ARC031_Bを追加

---
+++ AtCoderCpp/2.0.5ARC031_B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define req(i, n) for(int i=0; i<n; i++)
+
+using namespace std;
+
+int main() {
+
+	vector<string> a(10);
+
+	req(i, 10) cin >> a.at(i);
+
+	
+
+
+
+
+
+	return 0;
+}
--- AtCoderCpp/314_B.cpp
+++ AtCoderCpp/314_B.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int main() {
+int main_314B() {
 
 	int n;
 	cin >> n;
