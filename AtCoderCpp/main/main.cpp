583ff7454bcfe1a2375f5714f22c00c0580680dc
katsuya-ominami <katsuya.ominami@gmail.com>
2023-10-04 21:45:35 +0900

    317_C AC

--- AtCoderCpp/main/main.cpp
+++ AtCoderCpp/main/main.cpp
@@ -16,10 +16,50 @@ using ull = unsigned long long;
 int di[] = { -1,0,1,0,-1,1,1,-1,0 }; // ↑ → ↓ ← 右上 右下 左下 左上  中心
 int dj[] = { 0,1,0,-1,1,1,-1,-1,0 };
 
+struct Edge {
+
+	// メンバ変数
+	int to;
+	int cost;
+
+	// コンストラクタ デフォルト引数をとる
+	// : コロン以降はメンバ初期化子リスト
+	Edge(int t=0, int c=0): to(t), cost(c){}
+
+};
+
+int solve() {
+
+}
+
 signed main() {
 
+	int n, m;
+	cin >> n >> m;
+	vector<vector<Edge>> g(n);
 	
+	rep(i, m) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		--a; --b;
+		g[a].emplace_back(b,c);
+		g[b].emplace_back(a, c);
+	}
 
+	int ans = 0;
+	auto f = [&](auto f, int v, vector<bool> visited, int dist) -> void {
+		visited[v] = true;
+		ans = max(ans, dist);
+		for (auto e : g[v]) {
+			if (visited[e.to]) continue;
+			f(f, e.to, visited, dist + e.cost);
+		}
+	};
+
+	rep(i, n) {
+		f(f, i, vector<bool>(n), 0);
+	};
+	cout << ans << endl;
 	return 0;
 
 }
